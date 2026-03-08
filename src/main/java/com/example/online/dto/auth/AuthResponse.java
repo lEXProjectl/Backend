c6df6242d88f5c280916731c607af533e2f7739f
Бунтукова Светлана <nigihayami_kohaku@mail.ru>
2026-03-08 20:23:44 +0300

    добавлен AuthResponse DTO

---
+++ src/main/java/com/example/online/dto/auth/AuthResponse.java
@@ -0,0 +1,19 @@
+package com.example.online.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private String phone;
+    private String role;
+    private Long userId;
+}
+
