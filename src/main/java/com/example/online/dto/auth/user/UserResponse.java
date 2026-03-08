f2bb44bd663fe95335f6ec65545107c184e7fe9f
Бунтукова Светлана <nigihayami_kohaku@mail.ru>
2026-03-08 20:40:01 +0300

    добавлен DTO для ответа с данными пользователя

---
+++ src/main/java/com/example/online/dto/auth/user/UserResponse.java
@@ -0,0 +1,25 @@
+package com.example.online.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+
+    private Long id;
+    private String name;
+    private String phone;
+    private String role;
+    private String avatar;
+    private Double rating;
+    private LocalDateTime createdAt;
+}
+
+
